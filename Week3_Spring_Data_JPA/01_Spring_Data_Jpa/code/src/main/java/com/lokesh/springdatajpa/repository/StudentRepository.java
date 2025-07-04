f84639a9c35ea22328c1be64af6fa8cd802af5a4
LOKESH REDDY MADDIREDDY <135021758+LovelyLoki@users.noreply.github.com>
2025-07-04 21:03:25 +0530

    Adding week 3 hands-on
    Spring Data JPA with Spring Boot, Hibernate

---
+++ Week3_Spring_Data_JPA/01_Spring_Data_Jpa/code/src/main/java/com/lokesh/springdatajpa/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.lokesh.springdatajpa.repository;
+
+import com.lokesh.springdatajpa.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByName(String name);
+}
